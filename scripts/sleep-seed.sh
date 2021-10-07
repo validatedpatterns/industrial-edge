ade82806b1a614459d85c427043114d2ca6b05fc
Martin Jackson <martjack@redhat.com>
2021-10-07 10:23:00 -0500

    Normalize whitespace

--- scripts/sleep-seed.sh
+++ scripts/sleep-seed.sh
@@ -3,8 +3,8 @@
 while [ 1 ]; do
 	echo "Waiting for seed resources to be ready in manuela-ci"
 	oc get -n manuela-ci pipeline seed 1>/dev/null 2>/dev/null && \
-    oc get -n manuela-ci task tkn 1>/dev/null 2>/dev/null && \
-    echo "Bootstrap seed now running" && break;
+	oc get -n manuela-ci task tkn 1>/dev/null 2>/dev/null && \
+	echo "Bootstrap seed now running" && break;
 	sleep 5;
 done
 
