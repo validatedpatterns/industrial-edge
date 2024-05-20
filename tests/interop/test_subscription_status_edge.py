e0cb2fcc7b7f73088017dce6216e45f6509cc276
Yukin Chan <yuqchen@redhat.com>
2024-05-20 17:15:06 +0800

    Move common test codes to validatedpatterns_tests package

--- tests/interop/test_subscription_status_edge.py
+++ tests/interop/test_subscription_status_edge.py
@@ -1,9 +1,7 @@
 import logging
 
 import pytest
-from ocp_resources.cluster_version import ClusterVersion
-from ocp_resources.subscription import Subscription
-from openshift.dynamic.exceptions import NotFoundError
+from validatedpatterns_tests.interop import subscription
 
 from . import __loggername__
 
@@ -21,87 +19,19 @@ def test_subscription_status_edge(openshift_dyn_client):
         "seldon-operator-certified": ["openshift-operators"],
     }
 
-    operator_versions = []
-    missing_subs = []
-    unhealthy_subs = []
-    missing_installplans = []
-    upgrades_pending = []
-
-    for key in expected_subs.keys():
-        for val in expected_subs[key]:
-            logger.info(f"Checking subscription {key} in {val} namespace")
-
-            try:
-                subs = Subscription.get(
-                    dyn_client=openshift_dyn_client, name=key, namespace=val
-                )
-                sub = next(subs)
-            except NotFoundError:
-                missing_subs.append(f"{key} in {val} namespace")
-                continue
-
-            logger.info(
-                f"State for {sub.instance.metadata.name}:"
-                f" {sub.instance.status.state}"
-            )
-            if sub.instance.status.state == "UpgradePending":
-                upgrades_pending.append(
-                    f"{sub.instance.metadata.name} in"
-                    f" {sub.instance.metadata.namespace} namespace"
-                )
-
-            logger.info(
-                "CatalogSourcesUnhealthy:"
-                f" {sub.instance.status.conditions[0].status}"
-            )
-            if sub.instance.status.conditions[0].status != "False":
-                logger.info(f"Subscription {sub.instance.metadata.name} is unhealthy")
-                unhealthy_subs.append(
-                    f"{sub.instance.metadata.name} in"
-                    f" {sub.instance.metadata.namespace} namespace"
-                )
-            else:
-                operator_versions.append(
-                    f"installedCSV: {sub.instance.status.installedCSV}"
-                )
-
-            logger.info(f"installPlanRef: {sub.instance.status.installPlanRef}")
-            if not sub.instance.status.installPlanRef:
-                logger.info(
-                    "No install plan found for subscription"
-                    f" {sub.instance.metadata.name} in"
-                    f" {sub.instance.metadata.namespace} namespace"
-                )
-                missing_installplans.append(
-                    f"{sub.instance.metadata.name} in"
-                    f" {sub.instance.metadata.namespace} namespace"
-                )
-
-            logger.info("")
-
-    if missing_subs:
-        logger.error(f"FAIL: The following subscriptions are missing: {missing_subs}")
-    if unhealthy_subs:
-        logger.error(
-            "FAIL: The following subscriptions are unhealthy:" f" {unhealthy_subs}"
-        )
-    if missing_installplans:
-        logger.error(
-            "FAIL: The install plan for the following subscriptions is"
-            f" missing: {missing_installplans}"
-        )
-    if upgrades_pending:
-        logger.error(
-            "FAIL: The following subscriptions are in UpgradePending state:"
-            f" {upgrades_pending}"
-        )
+    (
+        operator_versions,
+        missing_subs,
+        unhealthy_subs,
+        missing_installplans,
+        upgrades_pending,
+    ) = subscription.subscription_status(openshift_dyn_client, expected_subs)
 
     for line in operator_versions:
         logger.info(line)
 
-    versions = ClusterVersion.get(dyn_client=openshift_dyn_client)
-    version = next(versions)
-    logger.info(f"Openshift version:\n{version.instance.status.history}")
+    cluster_version = subscription.openshift_version(openshift_dyn_client)
+    logger.info(f"Openshift version:\n{cluster_version.instance.status.history}")
 
     if missing_subs or unhealthy_subs or missing_installplans or upgrades_pending:
         err_msg = "Subscription status check failed"
--- tests/interop/test_validate_hub_site_components.py
+++ tests/interop/test_validate_hub_site_components.py
@@ -3,15 +3,18 @@ import os
 import subprocess
 
 import pytest
-from ocp_resources.namespace import Namespace
 from ocp_resources.pod import Pod
 from ocp_resources.route import Route
 from ocp_resources.storage_class import StorageClass
 from openshift.dynamic.exceptions import NotFoundError
+from validatedpatterns_tests.interop import components
+from validatedpatterns_tests.interop.crd import ArgoCD, ManagedCluster
+from validatedpatterns_tests.interop.edge_util import (
+    get_long_live_bearer_token,
+    get_site_response,
+)
 
 from . import __loggername__
-from .crd import ArgoCD, ManagedCluster
-from .edge_util import get_long_live_bearer_token, get_site_response
 
 logger = logging.getLogger(__loggername__)
 
@@ -50,13 +53,11 @@ hub site (central server):
 @pytest.mark.test_validate_hub_site_components
 def test_validate_hub_site_components(openshift_dyn_client):
     logger.info("Checking Openshift version on hub site")
-    version_out = subprocess.run([oc, "version"], capture_output=True)
-    version_out = version_out.stdout.decode("utf-8")
+    version_out = components.dump_openshift_version()
     logger.info(f"Openshift version:\n{version_out}")
 
     logger.info("Dump PVC and storageclass info")
-    pvcs_out = subprocess.run([oc, "get", "pvc", "-A"], capture_output=True)
-    pvcs_out = pvcs_out.stdout.decode("utf-8")
+    pvcs_out = components.dump_pvc()
     logger.info(f"PVCs:\n{pvcs_out}")
 
     for sc in StorageClass.get(dyn_client=openshift_dyn_client):
@@ -95,9 +96,6 @@ def test_check_pod_status(openshift_dyn_client, altprojects):
     logger.info("Checking pod status")
 
     err_msg = []
-    failed_pods = []
-    missing_pods = []
-    missing_projects = []
 
     if altprojects is None:
         projects = [
@@ -115,65 +113,14 @@ def test_check_pod_status(openshift_dyn_client, altprojects):
     else:
         projects = [altprojects]
 
-    for project in projects:
-        # Check for missing project
-        try:
-            namespaces = Namespace.get(dyn_client=openshift_dyn_client, name=project)
-            next(namespaces)
-        except NotFoundError:
-            missing_projects.append(project)
-            continue
-        # Check for absence of pods in project
-        try:
-            pods = Pod.get(dyn_client=openshift_dyn_client, namespace=project)
-            pod = next(pods)
-        except StopIteration:
-            missing_pods.append(project)
-            continue
+    missing_projects = components.check_project_absense(openshift_dyn_client, projects)
+    missing_pods = []
+    failed_pods = []
 
     for project in projects:
-        pods = Pod.get(dyn_client=openshift_dyn_client, namespace=project)
         logger.info(f"Checking pods in namespace '{project}'")
-        for pod in pods:
-            # Check jupyterhub pods seperately
-            if "jupyter" in pod.instance.metadata.name:
-                logger.info(f"Skipping check for pod: {pod.instance.metadata.name}")
-                continue
-
-            for container in pod.instance.status.containerStatuses:
-                logger.info(
-                    f"{pod.instance.metadata.name} : {container.name} :"
-                    f" {container.state}"
-                )
-                if container.state.terminated:
-                    if container.state.terminated.reason != "Completed":
-                        logger.info(
-                            f"Pod {pod.instance.metadata.name} in"
-                            f" {pod.instance.metadata.namespace} namespace is"
-                            " FAILED:"
-                        )
-                        failed_pods.append(pod.instance.metadata.name)
-                        logger.info(describe_pod(project, pod.instance.metadata.name))
-                        logger.info(
-                            get_log_output(
-                                project,
-                                pod.instance.metadata.name,
-                                container.name,
-                            )
-                        )
-                elif not container.state.running:
-                    logger.info(
-                        f"Pod {pod.instance.metadata.name} in"
-                        f" {pod.instance.metadata.namespace} namespace is"
-                        " FAILED:"
-                    )
-                    failed_pods.append(pod.instance.metadata.name)
-                    logger.info(describe_pod(project, pod.instance.metadata.name))
-                    logger.info(
-                        get_log_output(
-                            project, pod.instance.metadata.name, container.name
-                        )
-                    )
+        missing_pods += components.check_pod_absence(openshift_dyn_client, project)
+        failed_pods += components.check_pod_status(openshift_dyn_client, projects)
 
     if missing_projects:
         err_msg.append(f"The following namespaces are missing: {missing_projects}")
@@ -191,26 +138,6 @@ def test_check_pod_status(openshift_dyn_client, altprojects):
         assert False, err_msg
     else:
         logger.info("PASS: Pod status check succeeded.")
-
-
-def describe_pod(project, pod):
-    cmd_out = subprocess.run(
-        [oc, "describe", "pod", "-n", project, pod], capture_output=True
-    )
-    if cmd_out.stdout:
-        return cmd_out.stdout.decode("utf-8")
-    else:
-        assert False, cmd_out.stderr
-
-
-def get_log_output(project, pod, container):
-    cmd_out = subprocess.run(
-        [oc, "logs", "-n", project, pod, "-c", container], capture_output=True
-    )
-    if cmd_out.stdout:
-        return cmd_out.stdout.decode("utf-8")
-    else:
-        assert False, cmd_out.stderr
 
 
 # No longer needed for ACM 2.7
